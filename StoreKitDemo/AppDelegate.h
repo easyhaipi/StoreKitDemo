5db34dbea9bd3039c7e2e00ae4fb65f5614ecc12
easyhaipi <easy_qingsong@163.com>
2017-02-16 13:54:29 +0800

    Initial Commit

---
+++ StoreKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreKitDemo
+//
+//  Created by taobaichi on 2017/2/16.
+//  Copyright © 2017年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreKitDemo
+//
+//  Created by taobaichi on 2017/2/16.
+//  Copyright © 2017年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
